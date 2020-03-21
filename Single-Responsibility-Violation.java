90ee07d144e3cd200016e8190a3ccb6d40e49105
Amoda Sasmitha <44108942+amoda-sasmitha@users.noreply.github.com>
2020-03-21 09:39:30 +0530

    Create Single-Responsibility-Violation.java

---
+++ Single-Responsibility-Violation.java
@@ -0,0 +1,26 @@
+public class ServiceImpl {
+
+    public void insertEmployee(Employee employee){
+        // insert employee
+    }
+
+    public void updateEmployee(Employee employee){
+        // update employee
+    }
+
+    public void deleteEmployee(Employee employee){
+        // delete employee
+    }
+
+    public void insertDepartment(Department department){
+        // insert department
+    }
+
+    public void updateDepartment(Department department){
+        // update department
+    }
+
+    public void deleteDepartment(Department department){
+        // delete department
+    }
+}
